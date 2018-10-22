82425febb1481406e29dc238d1bdab12c7ceb3dd
Clumpy Tuna <anuhinm@mail.ru>
2018-10-22 14:41:51 +0300

    Add BadSamples

---
+++ BadSamples/LC_1.java
@@ -0,0 +1,18 @@
+// The classes are basically the same as the BinaryTree 
+// file except the visitor classes and the accept method
+// in the Tree class
+
+class TreeVisitor{
+    public static void main(String[] a){
+	System.out.println(new TV().Start());
+    }
+}
+
+class TV {
+
+    public int Start(){
+		Здравствуй, мир!
+	return 0 ;
+    }
+
+}
