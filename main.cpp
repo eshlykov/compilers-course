1a5da68a83a580c50d0dff3371386be9299a7a04
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-10-22 22:17:05 +0300

    Fix main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include "ast/expression.hpp"
-#include "visitor/interpreter.hpp"
+#include "ast/class-declaration.hpp"
+#include "ast/class-declaration-repeated.hpp"
+#include "ast/comma-type-identifier.hpp"
+#include "ast/comma-type-identifier-repeated.hpp"
+#include "ast/extends-identifier-optional.hpp"
+#include "ast/goal.hpp"
+#include "ast/main-class.hpp"
+#include "ast/method-declaration.hpp"
+#include "ast/type-identifier-comma-type-identifier-repeated.hpp"
+#include "ast/type-identifier-comma-type-identifier-repeated-optional.hpp"
+#include "ast/var-declaration.hpp"
+#include "ast/var-declaration-repeated.hpp"
 
 extern int yyparse();
 
 int main() {
-    std::shared_ptr<IVisitor> visitor{new Interpreter{}};
-    std::shared_ptr<ExpressionAndExpression> expression{new ExpressionAndExpression{true}};
-//    yyparse();
+    yyparse();
 }
