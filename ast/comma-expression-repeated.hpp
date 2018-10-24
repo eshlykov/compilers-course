0fb5aa2577e98d185e31e823d41e521ec0e0d939
svinkapeppa <erubanenko@gmail.com>
2018-10-24 22:39:14 +0300

    Add `CommaExpressionRepeated` class for AST
    Changes:
    * Add `.hpp` file

---
+++ ast/comma-expression-repeated.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class CommaExpression;
+
+class CommaExpressionRepeated {
+public:
+    explicit CommaExpressionRepeated(const std::vector<std::shared_ptr<CommaExpression>>& commaExpression) :
+        commaExpression_{commaExpression} {
+    }
+public:
+    std::vector<std::shared_ptr<CommaExpression>> commaExpression_ = {};
+};
