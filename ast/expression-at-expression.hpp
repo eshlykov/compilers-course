4413fddbdbd260762eb6f74a9416ffa8fad2d9d4
svinkapeppa <erubanenko@gmail.com>
2018-10-24 23:54:40 +0300

    Refactor
    Changes:
    * Remove to `ExpressionAtExpression`
    * Add {} to fields

---
+++ ast/expression-at-expression.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "expression.h"
+#include <memory>
+
+class ExpressionAtExpression : public Expression {
+public:
+    ExpressionAtExpression(std::shared_ptr<Expression> lhs, std::shared_ptr<Expression> rhs) :
+        lhs_{lhs}, rhs_{rhs} {
+    }
+
+public:
+    std::shared_ptr<Expression> lhs_ = {};
+    std::shared_ptr<Expression> rhs_ = {};
+};
