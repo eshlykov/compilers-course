12a5572807a670a843bcfd7c9126d7b80c14e27b
Clumpy Tuna <anuhinm@mail.ru>
2018-10-24 23:24:36 +0300

    Fix

--- ast/statement-repeated.hpp
+++ ast/statement-repeated.hpp
@@ -9,8 +9,8 @@ class StatementRepeated {
 public:
     StatementRepeated(const std::vector<std::shared_ptr<Statement>>& statementRepeated) :
         statementRepeated_{statementRepeated} {
-
     }
+
 public:
     std::vector<std::shared_ptr<Statement>> statementRepeated_ = {};
 };
