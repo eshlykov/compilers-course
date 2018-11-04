dcc8d9bb032e9fb073bf01898c30f89a8af87fbe
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-04 11:18:14 +0300

    MethodBody

---
+++ ast/method-body.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../visitor.hpp"
+#include "node.hpp"
+
+class VarDeclaration;
+class Statement;
+class Expression;
+
+class MethodBody : public Node {
+public:
+    MethodBody(const std::vector<VarDeclaration*>& variables, const std::vector<Statement*>& statements, Expression* returnExpression) :
+        variables_{variables}, statements_{statements}, returnExpression_{returnExpression} {
+    }
+
+    virtual void Accept(Visitor* visitor) override final {
+        visitor->Visitor(this);
+    }
+
+public:
+    std::vector<VarDeclaration*> variables_ = {};
+    std::vector<Statement*> statements_ = {};
+    Expression* returnExpression_ = {};
+};
