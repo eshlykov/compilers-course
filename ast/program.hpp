6ff8af69a5a4042c3a599943bef33a8e953b4dbb
svinkapeppa <erubanenko@gmail.com>
2018-11-04 10:55:18 +0300

    Add Program class

---
+++ ast/program.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../visitor.hpp"
+#include "node.hpp"
+#include <vector>
+
+class MainClass;
+class ClassDeclaration;
+
+class Program : public Node {
+public:
+    Program(MainClass* mainClass, const std::vector<ClassDeclaration*>& classDeclarations) :
+        mainClass_{mainClass}, classDeclarations_{classDeclarations} {
+    }
+
+    virtual void Accept(Visitor* visitor) override final {
+        visitor->Visit(this);
+    }
+
+public:
+    MainClass* mainClass_ = {};
+    std::vector<ClassDeclaration*> classDeclarations = {};
+};
