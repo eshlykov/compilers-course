e13a9ec10471aa89e2959e8c502e902baee8991a
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-04 11:58:17 +0300

    AssignmentStatement

---
+++ ast/assignment-statement.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../visitor.hpp"
+#include "statement.hpp"
+#include <string>
+
+class Expression;
+
+class AssignmentStatement : public Statement {
+public:
+    AssignmentStatement(const std::string& variable, Expression* expression) :
+        variable_{variable}, expression_(expression) {
+    }
+
+    virtual void Accept(Visitor* visitor) override final {
+        visitor->Visit(this);
+    }
+
+public:
+    std::string variable_ = {};
+    Expression* expression_ = {};
+};
