4b4612787414c25a1f18a54b049a4676555eba2e
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-04 11:32:19 +0300

    PrimitiveType

---
+++ ast/primitive-type.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../visitor.hpp"
+#include "type.hpp"
+
+enum class TypeKind {
+    TK_IntArray,
+    TK_Boolean,
+    TK_Int,
+};
+
+class PrimitiveType : public Type {
+public:
+    PrimitiveType(TypeKind typeKind) :
+        typeKind_{typeKind} {
+    }
+
+    virtual void Accept(Visitor* visitor) override final {
+        visitor->Visit(this);
+    }
+
+public:
+    TypeKind typeKind_ = {};
+};
