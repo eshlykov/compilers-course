bc6c0e785cc65183eaf414afe297ffac85e25d4e
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-04 11:53:44 +0300

    LoopStatement

---
+++ ast/loop-statement.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../visitor.hpp"
+#include "statement.hpp"
+
+class Expression;
+
+class LoopStatement : public Statement {
+public:
+    LoopWhile(Expression* condition, Statement* statement) :
+        condition_(condition), statement_(statement) {
+    }
+
+    virtual void Accept(Visitor* visitor) override final {
+        visitor->Visit(this);
+    }
+
+public:
+    Expression* condition_ = {};
+    Statement* statement_ = {};
+};
