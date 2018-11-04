1e781dd0b69a00b61c3b6eaeb85def7cb8690056
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-04 13:28:51 +0300

    Remove syntax tree

--- st/expression.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "../visitor.hpp"
-#include "node.hpp"
-
-class Expression : public Node {
-};
--- st/type.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "node.hpp"
-
-class Type : public Node {
-};
