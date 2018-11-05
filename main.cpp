60461c3ee02ec19af1cd5d667f55dd641608f63c
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-05 14:06:23 +0300

    Add symbol table

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "ast/program.hpp"
+#include "compile-error.hpp"
 #include "visitor/printer.hpp"
 
 extern int yyparse(Program*&);
@@ -9,6 +10,15 @@ int main() {
 
     Printer printer{"ast.dot"};
     printer.Visit(program);
+
+    SymbolTable symbolTable{};
+    try {
+        symbolTable.Visit(program);
+    } catch (const CompileError& error) {
+        std::cout << error.what() << std::endl;
+        delete program;
+        std::exit(1);
+    }
 
     delete program;
 }
