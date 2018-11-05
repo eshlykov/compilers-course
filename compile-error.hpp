84a7da24319df5536d480c059fffdcf815382912
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-05 13:35:01 +0300

    Add MethodRedefinition

---
+++ compile-error.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <exception>
+
+class CompileError : public std::exception {
+public:
+    CompileError(const std::string& message) :
+        message_{message} {
+    }
+
+    virtual const char* what() override final {
+        return message_.c_str();
+    }
+
+private:
+    std::string message_ = {};
+};
+
+class ClassRedefinition : public CompileError {
+public:
+    ClassRedefinition(const std::string& message) :
+        CompileError(message) {
+    }
+};
+
+class VariableRedefition : public CompileError {
+public:
+    VariableRefenition(const std::string& message) :
+        CompileError(message) {
+    }
+};
+
+class MethodRedefintion : public CompileError {
+public:
+    MethodRedefintion(const std::string& message) :
+        CompileError(message) {
+    }
+};
