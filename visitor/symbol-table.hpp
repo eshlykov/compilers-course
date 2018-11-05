4b84991ef35af8176ac6bf3a8627d4018853ef64
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-05 13:33:17 +0300

    Add exceptions

--- visitor/symbol-table.hpp
+++ visitor/symbol-table.hpp
@@ -6,15 +6,17 @@
 class ClassInfo {
 public:
     void AddVariable(const std::string& name, VariableInfo variable) {
-        if (variables_.find(name) == variables_.end()) {
-            variables_[name] = variable;
+        if (variables_.find(name) != variables_.end()) {
+            throw VariableRedefinition{"Variable " + name + " has been already defined."};
         }
+        variables_[name] = variable;
     }
 
     void AddMethod(const std::string& name, MethodInfo method) {
-        if (methods_.find(name) == methods_.end()) {
-            methods_[name] = method;
+        if (methods_.find(name) != methods_.end()) {
+            throw MethodRedefinition{"Method " + name + " has been already defined."};
         }
+        methods_[name] = method;
     }
 
 public:
