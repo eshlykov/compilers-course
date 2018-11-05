27bf4d10870c298852de5385585874dd810e77db
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-05 13:12:19 +0300

    Add SymbolTable

--- visitor/symbol-table.hpp
+++ visitor/symbol-table.hpp
@@ -5,8 +5,6 @@
 
 class ClassInfo {
 public:
-    ClassInfo() = default;
-
     void AddVariable(const std::string& name, VariableInfo variable) {
         if (variables_.find(name) == variables_.end()) {
             variables_[name] = variable;
@@ -23,3 +21,90 @@ public:
     std::unordered_map<std::string, VariableInfo> variables_ = {};
     std::unordered_map<std::string, MethodInfo> methods_ = {};
 };
+
+class SymbolTable : public Visitor {
+public:
+    virtual void Visit(AssignmentByIndexStatement*) override final {
+    }
+
+    virtual void Visit(AssignmentStatement*) override final {
+    }
+
+    virtual void Visit(BinaryOperatorExpression*) override final {
+    }
+
+    virtual void Visit(BooleanExpression*) override final {
+    }
+
+    virtual void Visit(ClassBody*) override final {
+    }
+
+    virtual void Visit(ClassDeclaration*) override final {
+    }
+
+    virtual void Visit(ConditionStatement*) override final {
+    }
+
+    virtual void Visit(IdentifierExpression*) override final {
+    }
+
+    virtual void Visit(IndexExpression*) override final {
+    }
+
+    virtual void Visit(IntArrayConstructorExpression*) override final {
+    }
+
+    virtual void Visit(LengthExpression*) override final {
+    }
+
+    virtual void Visit(LoopStatement*) override final {
+    }
+
+    virtual void Visit(MainClass*) override final {
+    }
+
+    virtual void Visit(MethodBody*) override final {
+    }
+
+    virtual void Visit(MethodCallExpression*) override final {
+    }
+
+    virtual void Visit(MethodDeclaration*) override final {
+    }
+
+    virtual void Visit(NotExpression*) override final {
+    }
+
+    virtual void Visit(NumberExpression*) override final {
+    }
+
+    virtual void Visit(ParensExpression*) override final {
+    }
+
+    virtual void Visit(PrimitiveType*) override final {
+    }
+
+    virtual void Visit(PrintStatement*) override final {
+    }
+
+    virtual void Visit(Program*) override final {
+    }
+
+    virtual void Visit(ScopeStatement*) override final {
+    }
+
+    virtual void Visit(ThisExpression*) override final {
+    }
+
+    virtual void Visit(UserTypeConstructorExpression*) override final {
+    }
+
+    virtual void Visit(UserType*) override final {
+    }
+
+    virtual void Visit(VarDeclaration*) override final {
+    }
+
+public:
+    std::unordered_map<std::string, ClassInfo> classes_ = {};
+}
