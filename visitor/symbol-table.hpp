296edb5c43c17c58cc89704eb0986430d6bfa4ac
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-05 13:30:49 +0300

    Add some exceptions

--- visitor/symbol-table.hpp
+++ visitor/symbol-table.hpp
@@ -18,91 +18,100 @@ public:
     }
 
 public:
+    std::optional<std::string> base_ = {};
     std::unordered_map<std::string, VariableInfo> variables_ = {};
     std::unordered_map<std::string, MethodInfo> methods_ = {};
 };
 
 class SymbolTable : public Visitor {
 public:
-    virtual void Visit(AssignmentByIndexStatement*) override final {
+    virtual void Visit(AssignmentByIndexStatement* node) override final {
     }
 
-    virtual void Visit(AssignmentStatement*) override final {
+    virtual void Visit(AssignmentStatement* node) override final {
     }
 
-    virtual void Visit(BinaryOperatorExpression*) override final {
+    virtual void Visit(BinaryOperatorExpression* node) override final {
     }
 
-    virtual void Visit(BooleanExpression*) override final {
+    virtual void Visit(BooleanExpression* node) override final {
     }
 
-    virtual void Visit(ClassBody*) override final {
+    virtual void Visit(ClassBody* node) override final {
     }
 
-    virtual void Visit(ClassDeclaration*) override final {
+    virtual void Visit(ClassDeclaration* node) override final {
+        if (classes_.find(node->className_) != classes_.end()) {
+            throw ClassRedefinition{"Class " + node->className_ + " has been already defined."};
+        }
+        auto& current = classes_[node->className_];
+        current.base = node->extendsForClass_;
     }
 
-    virtual void Visit(ConditionStatement*) override final {
+    virtual void Visit(ConditionStatement* node) override final {
     }
 
-    virtual void Visit(IdentifierExpression*) override final {
+    virtual void Visit(IdentifierExpression* node) override final {
     }
 
-    virtual void Visit(IndexExpression*) override final {
+    virtual void Visit(IndexExpression* node) override final {
     }
 
-    virtual void Visit(IntArrayConstructorExpression*) override final {
+    virtual void Visit(IntArrayConstructorExpression* node) override final {
     }
 
-    virtual void Visit(LengthExpression*) override final {
+    virtual void Visit(LengthExpression* node) override final {
     }
 
-    virtual void Visit(LoopStatement*) override final {
+    virtual void Visit(LoopStatement* node) override final {
     }
 
-    virtual void Visit(MainClass*) override final {
+    virtual void Visit(MainClass* node) override final {
     }
 
-    virtual void Visit(MethodBody*) override final {
+    virtual void Visit(MethodBody* node) override final {
     }
 
-    virtual void Visit(MethodCallExpression*) override final {
+    virtual void Visit(MethodCallExpression* node) override final {
     }
 
-    virtual void Visit(MethodDeclaration*) override final {
+    virtual void Visit(MethodDeclaration* node) override final {
     }
 
-    virtual void Visit(NotExpression*) override final {
+    virtual void Visit(NotExpression* node) override final {
     }
 
-    virtual void Visit(NumberExpression*) override final {
+    virtual void Visit(NumberExpression* node) override final {
     }
 
-    virtual void Visit(ParensExpression*) override final {
+    virtual void Visit(ParensExpression* node) override final {
     }
 
-    virtual void Visit(PrimitiveType*) override final {
+    virtual void Visit(PrimitiveType* node) override final {
     }
 
-    virtual void Visit(PrintStatement*) override final {
+    virtual void Visit(PrintStatement* node) override final {
     }
 
-    virtual void Visit(Program*) override final {
+    virtual void Visit(Program* node) override final {
+        for (auto* classDeclaration : classDeclarations_) {
+            classDeclaration->Accept(this);
+        }
     }
 
-    virtual void Visit(ScopeStatement*) override final {
+    virtual void Visit(ScopeStatement* node) override final {
     }
 
-    virtual void Visit(ThisExpression*) override final {
+    virtual void Visit(ThisExpression* node) override final {
     }
 
-    virtual void Visit(UserTypeConstructorExpression*) override final {
+    virtual void Visit(UserTypeConstructorExpression* node) override final {
     }
 
-    virtual void Visit(UserType*) override final {
+    virtual void Visit(UserType* node) override final {
     }
 
-    virtual void Visit(VarDeclaration*) override final {
+    virtual void Visit(VarDeclaration* node) override final {
     }
 
 public:
