3bfff2fad60c9c78ab2006e735521db15ba602ea
Clumpy Tuna <anuhinm@mail.ru>
2018-11-11 13:21:17 +0300

    Fancy output

--- checker.py
+++ checker.py
@@ -9,13 +9,19 @@ compiler = sys.argv[1]
 
 def run_tests(directory, is_good):
     failures_count = 0
+    print('Run tests!')
+    print()
     for file in os.listdir(directory):
         if file.endswith('.java'):
             test = os.path.abspath(os.path.join(directory, file))
+            print('Test: {}'.format(file))
             return_code = subprocess.call('./{} < {}'.format(compiler, test), shell=True)
             if return_code != 0 and is_good or return_code == 0 and not is_good:
-                print('Test {} is failed'.format(file))
+                print('State: failed')
+                print('')
                 failures_count += 1
+                continue
+            print('State: passed')
     return failures_count
 
 false_negatives_count = run_tests('tests/samples-good/', True)
