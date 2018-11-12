2245b294d29abc41d05de5fa7476f1e8add834c0
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-11-12 17:46:27 +0300

    Remove empty lines

--- main.cpp
+++ main.cpp
@@ -19,8 +19,6 @@ std::string ParseDrawingFilenameFromArguments(int argc, char *argv[]) {
  * To use our compiler now you should provide name of .dot file as first argument
  */
 int main(int argc, char *argv[]) {
-
-
     std::unique_ptr<Program> program = nullptr;
     if (yyparse(program) != 0) {
         std::cout << "yyparse failed" << std::endl;
