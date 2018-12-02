921a42799960861fa638e3100488203437a359c6
svinkapeppa <erubanenko@gmail.com>
2018-12-02 14:24:41 +0300

    Fix wrong assert

--- ast/scope-statement.cpp
+++ ast/scope-statement.cpp
@@ -4,7 +4,6 @@ ScopeStatement::ScopeStatement(Location location,
     std::vector<std::unique_ptr<Statement>>& statements) :
         statements_{std::move(statements)} {
     SetLocation(location);
-    assert(statements_ != nullptr);
 }
 
 void ScopeStatement::Accept(Visitor* visitor) {
