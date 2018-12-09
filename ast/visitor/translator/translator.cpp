77c77aa2e36d9630b454c6f42cab41ae2ceb2745
Evgeny Shlykov <e.a.shlykov@gmail.com>
2018-12-09 20:18:18 +0300

    Add empty translator

---
+++ ast/visitor/translator/translator.cpp
@@ -0,0 +1,80 @@
+#include "translator.hpp"
+
+namespace Ast {
+
+    void Translator::Visit(AssignmentByIndexStatement* node) {
+    }
+
+    void Translator::Visit(AssignmentStatement* node) {
+    }
+
+    void Translator::Visit(BinaryOperatorExpression* node) {
+    }
+
+    void Translator::Visit(BooleanExpression* node) {
+    }
+
+    void Translator::Visit(ClassBody* node) {
+    }
+
+    void Translator::Visit(ClassDeclaration* node) {
+    }
+
+    void Translator::Visit(ConditionStatement* node) {
+    }
+
+    void Translator::Visit(IdentifierExpression* node) {
+    }
+
+    void Translator::Visit(IndexExpression* node) {
+    }
+
+    void Translator::Visit(IntArrayConstructorExpression* node) {
+    }
+
+    void Translator::Visit(LengthExpression* node) {
+    }
+
+    void Translator::Visit(LoopStatement* node) {
+    }
+
+    void Translator::Visit(MainClass* node) {
+    }
+
+    void Translator::Visit(MethodBody* node) {
+    }
+
+    void Translator::Visit(MethodCallExpression* node) {
+    }
+
+    void Translator::Visit(MethodDeclaration* node) {
+    }
+
+    void Translator::Visit(NotExpression* node) {
+    }
+
+    void Translator::Visit(NumberExpression* node) {
+    }
+
+    void Translator::Visit(PrintStatement* node) {
+    }
+
+    void Translator::Visit(Program* node) {
+    }
+
+    void Translator::Visit(ScopeStatement* node) {
+    }
+
+    void Translator::Visit(ThisExpression* node) {
+    }
+
+    void Translator::Visit(Type* node) {
+    }
+
+    void Translator::Visit(UserTypeConstructorExpression* node) {
+    }
+
+    void Translator::Visit(VarDeclaration* node) {
+    }
+
+}
---
+++ ast/visitor/translator/translator.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "../../../utils/ast.hpp"
+#include "../visitor.hpp"
+
+namespace Ast {
+
+    class Translator : public Visitor {
+    public:
+        virtual void Visit(AssignmentByIndexStatement* node) override final;
+
+        virtual void Visit(AssignmentStatement* node) override final;
+
+        virtual void Visit(BinaryOperatorExpression* node) override final;
+
+        virtual void Visit(BooleanExpression* node) override final;
+
+        virtual void Visit(ClassBody* node) override final;
+
+        virtual void Visit(ClassDeclaration* node) override final;
+
+        virtual void Visit(ConditionStatement* node) override final;
+
+        virtual void Visit(IdentifierExpression* node) override final;
+
+        virtual void Visit(IndexExpression* node) override final;
+
+        virtual void Visit(IntArrayConstructorExpression* node) override final;
+
+        virtual void Visit(LengthExpression* node) override final;
+
+        virtual void Visit(LoopStatement* node) override final;
+
+        virtual void Visit(MainClass* node) override final;
+
+        virtual void Visit(MethodBody* node) override final;
+
+        virtual void Visit(MethodCallExpression* node) override final;
+
+        virtual void Visit(MethodDeclaration* node) override final;
+
+        virtual void Visit(NotExpression* node) override final;
+
+        virtual void Visit(NumberExpression* node) override final;
+
+        virtual void Visit(PrintStatement* node) override final;
+
+        virtual void Visit(Program* node) override final;
+
+        virtual void Visit(ScopeStatement* node) override final;
+
+        virtual void Visit(ThisExpression* node) override final;
+
+        virtual void Visit(Type* node) override final;
+
+        virtual void Visit(UserTypeConstructorExpression* node) override final;
+
+        virtual void Visit(VarDeclaration* node) override final;
+    };
+
+}
