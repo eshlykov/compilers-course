6911669270588b4981e1aecd20ecc27d1aa6062d
svinkapeppa <erubanenko@gmail.com>
2018-12-17 15:14:11 +0300

    Add loop for code fragments

--- irt/visitor/printer/code-fragment-printer.cpp
+++ irt/visitor/printer/code-fragment-printer.cpp
@@ -4,12 +4,14 @@ namespace Irt {
 
     void Print(const std::string& filename, std::shared_ptr<CodeFragment> codeFragment) {
         Printer printer{filename};
-        auto body = std::make_shared<ExpressionSequence>(
-            codeFragment->body_,
-            std::make_shared<Constant>(0)
-        );
-        printer.Visit(body.get());
-        codeFragment = codeFragment->next_;
+        while (codeFragment != nullptr) {
+            auto body = std::make_shared<ExpressionSequence>(
+                codeFragment->body_,
+                std::make_shared<Constant>(0)
+            );
+            printer.Visit(body.get());
+            codeFragment = codeFragment->next_;
+        }
     }
 
 }
