15ef3e3eae44c66a2d71b59fdab3602b4bcf5bfc
svinkapeppa <erubanenko@gmail.com>
2019-04-04 17:46:08 +0300

    Blank Canonizer

---
+++ ct/visitor/canonizer/canonizer.cpp
@@ -0,0 +1,59 @@
+#include "canonizer.hpp"
+
+namespace Ct {
+
+    Canonizer() {
+    }
+
+    void Canonizer::Visit(BinaryOperator*) {
+
+    }
+
+    void Canonizer::Visit(Call*) {
+
+    }
+
+    void Canonizer::Visit(ConditionalJump*) {
+
+    }
+
+    void Canonizer::Visit(Constant*) {
+
+    }
+
+    void Canonizer::Visit(ExpressionSequence*) {
+
+    }
+
+    void Canonizer::Visit(Jump*) {
+
+    }
+
+    void Canonizer::Visit(Label*) {
+
+    }
+
+    void Canonizer::Visit(Memory*) {
+
+    }
+
+    void Canonizer::Visit(Move*) {
+
+    }
+
+    void Canonizer::Visit(Name*) {
+
+    }
+
+    void Canonizer::Visit(StatementSequence*) {
+
+    }
+
+    void Canonizer::Visit(Temporary*) {
+
+    }
+
+    void Canonizer::Visit(Void*) {
+
+    }
+}
---
+++ ct/visitor/canonizer/canonizer.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+namespace Ct {
+
+    class Canonizer : public Visitor {
+    public:
+        Canonizer();
+
+        virtual void Visit(BinaryOperator*) override final;
+
+        virtual void Visit(Call*) override final;
+
+        virtual void Visit(ConditionalJump*) override final;
+
+        virtual void Visit(Constant*) override final;
+
+        virtual void Visit(ExpressionSequence*) override final;
+
+        virtual void Visit(Jump*) override final;
+
+        virtual void Visit(Label*) override final;
+
+        virtual void Visit(Memory*) override final;
+
+        virtual void Visit(Move*) override final;
+
+        virtual void Visit(Name*) override final;
+
+        virtual void Visit(StatementSequence*) override final;
+
+        virtual void Visit(Temporary*) override final;
+
+        virtual void Visit(Void*) override final;
+
+    private:
+        std::shared_ptr<Node> node_;
+    };
+
+}
