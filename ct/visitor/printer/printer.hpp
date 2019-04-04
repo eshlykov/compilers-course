f78cfa8f5fe48e634334dac0b1710ac5387baece
Evgeny Shlykov <e.a.shlykov@gmail.com>
2019-04-04 17:30:56 +0300

    printer is ready

---
+++ ct/visitor/printer/printer.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "../../../utils/ct.hpp"
+#include "../visitor.hpp"
+#include <fstream>
+#include <string>
+
+namespace Ct {
+
+    class Printer : public Visitor {
+    public:
+        explicit Printer(const std::string& filename);
+
+        ~Printer();
+
+        virtual void Visit(BinaryOperator* node) override final;
+
+        virtual void Visit(Call* node) override final;
+
+        virtual void Visit(ConditionalJump*) override final;
+
+        virtual void Visit(Constant* node) override final;
+
+        virtual void Visit(ExpressionSequence*) override final;
+
+        virtual void Visit(Jump* node) override final;
+
+        virtual void Visit(Label* node) override final;
+
+        virtual void Visit(Memory* node) override final;
+
+        virtual void Visit(Move* node) override final;
+
+        virtual void Visit(Name* node) override final;
+
+        virtual void Visit(StatementSequence* node) override final;
+
+        virtual void Visit(Temporary* node) override final;
+
+        virtual void Visit(Void* node) override final;
+
+        void Next();
+
+    private:
+        void PrintHead(int headNodeNumber, const std::string& label);
+
+        void PrintEdge(int headNodeNumber);
+
+        void PrintLeaf(int headNodeNumber, const std::string& label, const std::string& name);
+
+    private:
+        std::ofstream file_;
+        int nodeNumber_;
+    };
+
+}
