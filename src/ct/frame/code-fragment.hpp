7b1f221a6295c143b6d8479e37c7a1bd2e39e506
Evgeny Shlykov <e.a.shlykov@gmail.com>
2019-04-07 21:41:31 +0300

    fix

--- src/ct/frame/code-fragment.hpp
+++ src/ct/frame/code-fragment.hpp
@@ -21,7 +21,7 @@ struct CodeFragment {
 inline CodeFragment::CodeFragment()
     : frame_{nullptr}, body_{nullptr}, next_{nullptr} {}
 
-CodeFragment::CodeFragment(const std::shared_ptr<Statement>& body, const std::shared_ptr<Irt::Frame>& frame)
+inline CodeFragment::CodeFragment(const std::shared_ptr<Statement>& body, const std::shared_ptr<Irt::Frame>& frame)
     : body_{std::dynamic_pointer_cast<StatementSequence>(body)}, frame_{std::make_shared<Frame>(frame)} {
   assert(body);
 }
--- src/ct/visitor/printer/code-fragment-printer.hpp
+++ src/ct/visitor/printer/code-fragment-printer.hpp
@@ -17,19 +17,19 @@ void Print(const std::string& filename,
     StatementSequencePtr body = codeFragment->body_;
     std::vector<StatementPtr> statements = linearizer.Linearize(body);
     for (auto&& statement : statements) {
-//      if (auto tmp = std::dynamic_pointer_cast<ConditionalJump>(statement); tmp != nullptr){
-//        printer.Visit(tmp.get());
-//      } else if (auto tmp = std::dynamic_pointer_cast<Jump>(statement); tmp != nullptr) {
-//        printer.Visit(tmp.get());
-//      } else if (auto tmp = std::dynamic_pointer_cast<Label>(statement); tmp != nullptr) {
-//        printer.Visit(tmp.get());
-//      } else if (auto tmp = std::dynamic_pointer_cast<Move>(statement); tmp != nullptr) {
-//        printer.Visit(tmp.get());
-//      } else if (auto tmp = std::dynamic_pointer_cast<StatementSequence>(statement); tmp != nullptr) {
-//        printer.Visit(tmp.get());
-//      } else if (auto tmp = std::dynamic_pointer_cast<Void>(statement); tmp != nullptr) {
-//        printer.Visit(tmp.get());
-//      }
+      if (auto tmp = std::dynamic_pointer_cast<ConditionalJump>(statement); tmp != nullptr){
+        printer.Visit(tmp.get());
+      } else if (auto tmp = std::dynamic_pointer_cast<Jump>(statement); tmp != nullptr) {
+        printer.Visit(tmp.get());
+      } else if (auto tmp = std::dynamic_pointer_cast<Label>(statement); tmp != nullptr) {
+        printer.Visit(tmp.get());
+      } else if (auto tmp = std::dynamic_pointer_cast<Move>(statement); tmp != nullptr) {
+        printer.Visit(tmp.get());
+      } else if (auto tmp = std::dynamic_pointer_cast<StatementSequence>(statement); tmp != nullptr) {
+        printer.Visit(tmp.get());
+      } else if (auto tmp = std::dynamic_pointer_cast<Void>(statement); tmp != nullptr) {
+        printer.Visit(tmp.get());
+      }
       printer.Next();
     }
     codeFragment = codeFragment->next_;
