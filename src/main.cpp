4e235d0a7724ffd845daad5e8af0440df3ffb6e4
Evgeny Shlykov <e.a.shlykov@gmail.com>
2019-06-13 18:16:57 +0300

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,24 @@
-#include <cstdio>
-#include <fstream>
 #include <iostream>
 #include <memory>
-#include "ast/node/program.hpp"
-#include "ast/visitor/printer/printer.hpp"
-#include "ast/visitor/symbol-table/symbol-table.hpp"
-#include "ast/visitor/translator/translator.hpp"
-#include "ct/visitor/printer/code-fragment-printer.hpp"
-#include "irt/visitor/printer/code-fragment-printer.hpp"
-#include "irt/visitor/translator/code-fragment-translator.hpp"
-#include "utils/compile-error/compile-error.hpp"
-#include "utils/parser-args.hpp"
-#include "utils/source-code.hpp"
+#include <stdexcept>
+#include <string>
 
-extern FILE* yyin;
-extern int yyparse(ParserArgs&);
+#include <utils/compiler.hpp>
 
-std::string ParseDrawingFilenameFromArguments(int argc, char* argv[]) {
-  if (argc == 3) {
-    return argv[2];
-  }
-  return "ast.dot";
+void PrintHelp(const std::string& programName) {
+  std::cerr << programName << " <inputFilePath> <outputDirPath>" << std::endl;
 }
 
-/*
- * To use our compiler now you should provide name of .dot file as first
- * argument
- */
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    std::cout << "no source filename is given" << std::endl;
-    return 1;
+  if (argc < 3) {
+    PrintHelp(argv[0]);
+    throw std::logic_error("Too few arguments provided");
   }
+  const std::string pathInputFile(argv[1]);
+  const std::string pathOutputDir(argv[2]);
 
-  std::string filename = argv[1];
-  SourceCode sourceCode{filename};
+  Compiler compiler(pathInputFile, pathOutputDir);
+  compiler.Run();
 
-  yyin = std::fopen(filename.c_str(), "r");
-  if (yyin == nullptr) {
-    std::cout << "cannot open file " << filename << std::endl;
-  }
-
-  ParserArgs parserArgs;
-  bool isParsed = yyparse(parserArgs) == 0;
-
-  std::fclose(yyin);
-
-  std::vector<CompileError> errors = parserArgs.errors_;
-  if (!errors.empty()) {
-    for (auto& error : errors) {
-      std::cout << error.GetMessage(sourceCode) << std::endl;
-    }
-    return 1;
-  }
-
-  if (!isParsed) {
-    std::cout << CompileError{"unexpected parser error", {}}.GetMessage(
-                     sourceCode)
-              << std::endl;
-    return 1;
-  }
-
-  std::unique_ptr<Ast::Program> program = std::move(parserArgs.program_);
-
-  if (program == nullptr) {
-    std::cout << CompileError{"program is null pointer", {}}.GetMessage(
-                     sourceCode)
-              << std::endl;
-    return 1;
-  }
-
-  // Ast::Printer astPrinter{ParseDrawingFilenameFromArguments(argc, argv)};
-  // astPrinter.Visit(program.get());
-
-  Ast::SymbolTable symbolTable{};
-  try {
-    symbolTable.Visit(program.get());
-  } catch (const std::exception& exception) {
-    std::cout << exception.what() << std::endl;
-    return 1;
-  } catch (...) {
-    std::cout << "unexpected error" << std::endl;
-    return 1;
-  }
-
-  if (auto redefinitions = symbolTable.GetErrorList(); !redefinitions.empty()) {
-    for (auto& error : redefinitions) {
-      std::cout << error.GetMessage(sourceCode) << std::endl;
-    }
-    return 1;
-  }
-
-  Ast::Translator translator;
-  translator.Visit(program.get());
-
-//    Irt::Print(ParseDrawingFilenameFromArguments(argc, argv),
-//               translator.GetCodeFragment());
-
-  std::shared_ptr<Ct::CodeFragment> codeFragment =
-      Irt::Translate(translator.GetCodeFragment());
-
-  Ct::Print(ParseDrawingFilenameFromArguments(argc, argv), codeFragment);
+  return 0;
 }
